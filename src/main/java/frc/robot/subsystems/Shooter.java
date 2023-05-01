7e93c8cc5ac6e56dc64659e8e956f2ec9e3a78a5
d2i-23 <clementcycc8@gmail.com>
2023-05-01 16:45:37 -0400

    sup

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new Shooter. */
+
+  CANSparkMax leftmotor;
+  CANSparkMax rightmotor; 
+
+  public Shooter() {
+    leftmotor = new CANSparkMax(0, null);
+    rightmotor = new CANSparkMax(0, null);
+    leftmotor.setInverted(true);
+  }
+
+
+  public void Shooting(){
+    leftmotor.set(0.8);
+    leftmotor.set(0.8);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
